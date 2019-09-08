0902909127d3fb3d7546daf75f19801d1a5c4ee8
Fatih Bulut <info@bulutfatih.com>
2019-09-08 22:23:18 +0300

    User model and authentication

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
+// Connect database
+connectDB();
+
+// Init middleware
+app.use(express.json({ extended: false }));
+
 app.get("/", (req, res) => res.send("API Running"));
+
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/posts", require("./routes/api/posts"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/users", require("./routes/api/users"));
 
 const PORT = process.env.PORT || 5000;
 
