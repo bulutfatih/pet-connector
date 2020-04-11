37310aa5bbd8985005f5acbf0ad29ba4d4b918e9
Fatih Bulut <info@bulutfatih.com>
2020-04-11 19:48:39 +0300

    Post like and unlike features are added.

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -3,9 +3,12 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import dayjs from "dayjs";
 import { connect } from "react-redux";
+import { addLike, removeLike } from "../../actions/post";
 
 const PostItem = (props) => {
   const {
+    addLike,
+    removeLike,
     auth,
     post: { _id, text, name, avatar, user, likes, comments, date },
   } = props;
@@ -23,11 +26,19 @@ const PostItem = (props) => {
         <p className="post-date">
           Posted on {dayjs(date).format("DD/MM/YYYY")}
         </p>
-        <button type="button" className="btn btn-light">
+        <button
+          onClick={(e) => addLike(_id)}
+          type="button"
+          className="btn btn-light"
+        >
           <i className="fas fa-thumbs-up"></i>
           {likes.length > 0 && <span> {likes.length}</span>}
         </button>
-        <button type="button" className="btn btn-light">
+        <button
+          onClick={(e) => removeLike(_id)}
+          type="button"
+          className="btn btn-light"
+        >
           <i className="fas fa-thumbs-down"></i>
         </button>
         <Link to={`/post/${_id}`} className="btn btn-primary">
@@ -55,6 +66,9 @@ const mapStateToProps = (state) => ({
   auth: state.auth,
 });
 
-const mapDispatchToProps = {};
+const mapDispatchToProps = {
+  addLike,
+  removeLike,
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(PostItem);
